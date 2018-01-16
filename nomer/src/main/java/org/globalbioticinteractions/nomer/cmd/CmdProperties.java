904477bcc4727876b6c24a13d89e815b39d5e466
jhpoelen <jhpoelen@xs4all.nl>
2018-01-16 13:15:56 -0800

    removed unused imports

--- nomer/src/main/java/org/globalbioticinteractions/nomer/cmd/CmdProperties.java
+++ nomer/src/main/java/org/globalbioticinteractions/nomer/cmd/CmdProperties.java
@@ -1,13 +1,10 @@
 package org.globalbioticinteractions.nomer.cmd;
 
 import com.beust.jcommander.Parameters;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import java.io.File;
 import java.util.Properties;
-import java.util.StringJoiner;
 
 @Parameters(separators = "= ", commandDescription = "Lists properties.")
 public class CmdProperties extends CmdDefaultParams {
