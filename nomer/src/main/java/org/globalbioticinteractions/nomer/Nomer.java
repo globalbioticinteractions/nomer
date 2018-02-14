20b86df636ec6e88ac81bd52b43835dad4654a79
jhpoelen <jhpoelen@xs4all.nl>
2018-02-13 16:17:16 -0800

    use package api to retrieve version

--- nomer/src/main/java/org/globalbioticinteractions/nomer/Nomer.java
+++ nomer/src/main/java/org/globalbioticinteractions/nomer/Nomer.java
@@ -4,19 +4,14 @@ package org.globalbioticinteractions.nomer;
     Nomer - a GloBI tool to help map identifiers and name to identifiers and names
  */
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.eol.globi.Version;
 import org.globalbioticinteractions.nomer.cmd.CmdLine;
 
 import static java.lang.System.exit;
 
 public class Nomer {
-    private static final Log LOG = LogFactory.getLog(Nomer.class);
 
     public static void main(String[] args) {
         try {
-            LOG.info(Version.getVersionInfo(Nomer.class));
             CmdLine.run(args);
             exit(0);
         } catch (Throwable t) {
--- nomer/src/main/java/org/globalbioticinteractions/nomer/cmd/CmdVersion.java
+++ nomer/src/main/java/org/globalbioticinteractions/nomer/cmd/CmdVersion.java
@@ -1,13 +1,12 @@
 package org.globalbioticinteractions.nomer.cmd;
 
 import com.beust.jcommander.Parameters;
-import org.eol.globi.Version;
 
 @Parameters(separators = "= ", commandDescription = "Show Version")
 public class CmdVersion implements Runnable {
 
     @Override
     public void run() {
-        System.out.println(Version.getVersion());
+        System.out.println(CmdVersion.class.getPackage().getImplementationVersion());
     }
 }
