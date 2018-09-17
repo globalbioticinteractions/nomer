9d0a001733af80eaeaeaac613a246f9de65bd8b3
jhpoelen <jhpoelen@xs4all.nl>
2018-09-17 14:32:46 -0700

    add jar magic

--- .travis.deploy_if_tag.sh
+++ .travis.deploy_if_tag.sh
@@ -2,5 +2,6 @@
 if [ -n "$TRAVIS_TAG" ]; then
   mvn -s .travis.maven.settings.xml -DskipTests clean deploy
   VERSION=${TRAVIS_TAG//[^0-9.]/}
-  cp nomer/target/nomer-${VERSION}-jar-with-dependencies.jar nomer.jar
+  cp nomer/target/nomer-${VERSION}-jar-with-dependencies.jar nomer_no_head.jar
+  cat .travis.jar.magic nomer_no_head.jar > nomer.jar
 fi
