da239f0469014e7cb7eaf600ede9bde62e7e9b01
jhpoelen <jhpoelen@xs4all.nl>
2020-05-19 14:24:00 -0700

    towards offline matching support for EOL pages; related to #11

---
+++ nomer-taxon-resolver/src/main/java/org/globalbioticinteractions/nomer/match/EOLTaxonService.java
@@ -0,0 +1,234 @@
+package org.globalbioticinteractions.nomer.match;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.StopWatch;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.eol.globi.data.CharsetConstant;
+import org.eol.globi.domain.PropertyAndValueDictionary;
+import org.eol.globi.domain.Taxon;
+import org.eol.globi.domain.TaxonImpl;
+import org.eol.globi.domain.TaxonomyProvider;
+import org.eol.globi.service.CacheService;
+import org.eol.globi.service.PropertyEnricher;
+import org.eol.globi.service.PropertyEnricherException;
+import org.eol.globi.service.TaxonUtil;
+import org.eol.globi.taxon.TaxonCacheService;
+import org.globalbioticinteractions.nomer.util.PropertyEnricherInfo;
+import org.globalbioticinteractions.nomer.util.TermMatcherContext;
+import org.mapdb.BTreeKeySerializer;
+import org.mapdb.BTreeMap;
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+@PropertyEnricherInfo(name = "eol-taxon-id", description = "Lookup EOL pages by id with EOL:* prefix using offline-enabled database dump")
+public class EOLTaxonService implements PropertyEnricher {
+
+    private static final Log LOG = LogFactory.getLog(EOLTaxonService.class);
+    private static final String DENORMALIZED_NODES = "denormalizedNodes";
+    private static final String MERGED_NODES = "mergedNodes";
+
+
+    private final TermMatcherContext ctx;
+
+    private BTreeMap<String, Map<String, String>> eolDenormalizedNodes = null;
+    private boolean temporaryCache = false;
+
+    public EOLTaxonService(TermMatcherContext ctx) {
+        this.ctx = ctx;
+    }
+
+    @Override
+    public Map<String, String> enrich(Map<String, String> properties) throws PropertyEnricherException {
+        Map<String, String> enriched = new TreeMap<>(properties);
+        String externalId = properties.get(PropertyAndValueDictionary.EXTERNAL_ID);
+        if (StringUtils.startsWith(externalId, TaxonomyProvider.ID_PREFIX_EOL)) {
+            if (needsInit()) {
+                if (ctx == null) {
+                    throw new PropertyEnricherException("context needed to initialize");
+                }
+                lazyInit();
+            }
+            Map<String, String> enrichedProperties = eolDenormalizedNodes.get(externalId);
+            enriched = enrichedProperties == null ? enriched : new TreeMap<>(enrichedProperties);
+        }
+        return enriched;
+    }
+
+
+    static void parseNodes(Map<String, Map<String, String>> taxonMap,
+                           Map<String, String> childParent,
+                           InputStream resourceAsStream) throws PropertyEnricherException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(resourceAsStream));
+
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                String[] rowValues = StringUtils.splitByWholeSeparatorPreserveAllTokens(line, "\t");
+                if (rowValues.length > 12) {
+                    String taxId = rowValues[0];
+                    String parentTaxId = rowValues[4];
+                    String rank = rowValues[7];
+
+                    String canonicalName = rowValues[11];
+                    String pageId = rowValues[12];
+
+                    String externalId = TaxonomyProvider.ID_PREFIX_EOL + pageId;
+                    TaxonImpl taxon = new TaxonImpl(canonicalName, externalId);
+                    taxon.setRank(rank);
+                    taxon.setExternalId(externalId);
+
+                    taxonMap.put(taxId, TaxonUtil.taxonToMap(taxon));
+
+                    childParent.put(
+                            taxId,
+                            parentTaxId
+                    );
+                }
+            }
+        } catch (IOException e) {
+            throw new PropertyEnricherException("failed to parse ITIS taxon dump", e);
+        }
+    }
+
+
+    static void denormalizeTaxa(Map<String, Map<String, String>> taxonMap, Map<String, Map<String, String>> taxonMapDenormalized, Map<String, String> childParent) {
+        Set<Map.Entry<String, Map<String, String>>> taxa = taxonMap.entrySet();
+        for (Map.Entry<String, Map<String, String>> taxon : taxa) {
+            denormalizeTaxa(taxonMap, taxonMapDenormalized, childParent, taxon);
+        }
+    }
+
+    private static void denormalizeTaxa(Map<String, Map<String, String>> taxonMap,
+                                        Map<String, Map<String, String>> taxonEnrichMap,
+                                        Map<String, String> childParent,
+                                        Map.Entry<String, Map<String, String>> taxon) {
+        Map<String, String> childTaxon = taxon.getValue();
+        List<String> pathNames = new ArrayList<>();
+        List<String> pathIds = new ArrayList<>();
+        List<String> path = new ArrayList<>();
+
+        Taxon origTaxon = TaxonUtil.mapToTaxon(childTaxon);
+
+        path.add(StringUtils.defaultIfBlank(origTaxon.getName(), ""));
+
+        String externalId = origTaxon.getExternalId();
+        pathIds.add(StringUtils.defaultIfBlank(externalId, ""));
+
+        pathNames.add(StringUtils.defaultIfBlank(origTaxon.getRank(), ""));
+
+        String parent = childParent.get(taxon.getKey());
+        while (StringUtils.isNotBlank(parent) && !pathIds.contains(parent)) {
+            Map<String, String> parentTaxonProperties = taxonMap.get(parent);
+            if (parentTaxonProperties != null) {
+                Taxon parentTaxon = TaxonUtil.mapToTaxon(parentTaxonProperties);
+                path.add(StringUtils.defaultIfBlank(parentTaxon.getName(), ""));
+                pathNames.add(StringUtils.defaultIfBlank(parentTaxon.getRank(), ""));
+                pathIds.add(StringUtils.defaultIfBlank(parentTaxon.getExternalId(), ""));
+            }
+            parent = childParent.get(parent);
+        }
+
+        Collections.reverse(pathNames);
+        Collections.reverse(pathIds);
+        Collections.reverse(path);
+
+        origTaxon.setPath(StringUtils.join(path, CharsetConstant.SEPARATOR));
+        origTaxon.setPathIds(StringUtils.join(pathIds, CharsetConstant.SEPARATOR));
+        origTaxon.setPathNames(StringUtils.join(pathNames, CharsetConstant.SEPARATOR));
+
+        taxonEnrichMap.put(externalId, TaxonUtil.taxonToMap(origTaxon));
+    }
+
+    private void lazyInit() throws PropertyEnricherException {
+        File cacheDir = getCacheDir(ctx);
+        if (temporaryCache) {
+            CacheService.createCacheDir(cacheDir);
+        } else {
+            if (!cacheDir.exists()) {
+                if (!cacheDir.mkdirs()) {
+                    throw new PropertyEnricherException("failed to create cache dir at [" + cacheDir.getAbsolutePath() + "]");
+                }
+            }
+        }
+
+        File taxonomyDir = new File(cacheDir, "eol");
+        DB db = DBMaker
+                .newFileDB(taxonomyDir)
+                .mmapFileEnableIfSupported()
+                .compressionEnable()
+                .closeOnJvmShutdown()
+                .transactionDisable()
+                .make();
+
+        if (db.exists(DENORMALIZED_NODES)) {
+            LOG.info("EOL taxonomy already indexed at [" + taxonomyDir.getAbsolutePath() + "], no need to import.");
+            eolDenormalizedNodes = db.getTreeMap(DENORMALIZED_NODES);
+        } else {
+            LOG.info("EOL taxonomy importing...");
+            StopWatch watch = new StopWatch();
+            watch.start();
+
+            BTreeMap<String, Map<String, String>> eolNodes = db
+                    .createTreeMap("nodes")
+                    .keySerializer(BTreeKeySerializer.STRING)
+                    .make();
+
+            BTreeMap<String, String> childParent = db
+                    .createTreeMap("childParent")
+                    .keySerializer(BTreeKeySerializer.STRING)
+                    .make();
+
+            try {
+                parseNodes(eolNodes, childParent, ctx.getResource(getTaxonUrl()));
+            } catch (IOException e) {
+                throw new PropertyEnricherException("failed to parse NCBI nodes", e);
+            }
+
+            eolDenormalizedNodes = db
+                    .createTreeMap(DENORMALIZED_NODES)
+                    .keySerializer(BTreeKeySerializer.STRING)
+                    .make();
+            denormalizeTaxa(eolNodes, eolDenormalizedNodes, childParent);
+
+            watch.stop();
+            TaxonCacheService.logCacheLoadStats(watch.getTime(), eolNodes.size(), LOG);
+            LOG.info("EOL taxonomy imported.");
+        }
+    }
+
+    private boolean needsInit() {
+        return eolDenormalizedNodes == null;
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+    private File getCacheDir(TermMatcherContext ctx) {
+        File cacheDir = new File(ctx.getCacheDir(), "eol");
+        cacheDir.mkdirs();
+        return cacheDir;
+    }
+
+    private String getTaxonUrl() throws PropertyEnricherException {
+        return ctx.getProperty("nomer.eol.taxon");
+    }
+
+    public void setTemporaryCache(boolean temporaryCache) {
+        this.temporaryCache = temporaryCache;
+    }
+}
---
+++ nomer-taxon-resolver/src/test/java/org/globalbioticinteractions/nomer/match/EOLTaxonServiceTest.java
@@ -0,0 +1,152 @@
+package org.globalbioticinteractions.nomer.match;
+
+import org.eol.globi.domain.Taxon;
+import org.eol.globi.domain.TaxonImpl;
+import org.eol.globi.service.PropertyEnricher;
+import org.eol.globi.service.PropertyEnricherException;
+import org.eol.globi.service.TaxonUtil;
+import org.globalbioticinteractions.nomer.util.TermMatcherContext;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.assertThat;
+
+public class EOLTaxonServiceTest {
+
+    @Test
+    public void enrich() throws PropertyEnricherException {
+        PropertyEnricher service = createService();
+
+        TaxonImpl taxon = new TaxonImpl(null, "EOL:327955");
+        Map<String, String> enriched = service.enrich(TaxonUtil.taxonToMap(taxon));
+
+        assertThat(TaxonUtil.mapToTaxon(enriched).getPath(), is("Homo | Homo sapiens"));
+        assertThat(TaxonUtil.mapToTaxon(enriched).getExternalId(), is("EOL:327955"));
+        assertThat(TaxonUtil.mapToTaxon(enriched).getName(), is("Homo sapiens"));
+        assertThat(TaxonUtil.mapToTaxon(enriched).getRank(), is("species"));
+        assertThat(TaxonUtil.mapToTaxon(enriched).getPathIds(), is("EOL:42268 | EOL:327955"));
+        assertThat(TaxonUtil.mapToTaxon(enriched).getPathNames(), is("genus | species"));
+    }
+
+
+    @Test
+    public void enrichNoMatch() throws PropertyEnricherException {
+        PropertyEnricher service = createService();
+
+        Map<String, String> enriched = service.enrich(TaxonUtil.taxonToMap(new TaxonImpl(null, "ITIS:999999999")));
+
+        assertThat(TaxonUtil.mapToTaxon(enriched).getPath(), is(nullValue()));
+    }
+
+    @Test
+    public void enrichPrefixMismatch() throws PropertyEnricherException {
+        PropertyEnricher service = createService();
+
+        Map<String, String> enriched = service.enrich(TaxonUtil.taxonToMap(new TaxonImpl(null, "FOO:2")));
+
+        assertThat(TaxonUtil.mapToTaxon(enriched).getPath(), is(nullValue()));
+    }
+
+    private PropertyEnricher createService() {
+        EOLTaxonService taxonService = new EOLTaxonService(new TermMatcherContext() {
+            @Override
+            public String getCacheDir() {
+                return null;
+            }
+
+            @Override
+            public InputStream getResource(String uri) throws IOException {
+                return getClass().getResourceAsStream(uri);
+            }
+
+            @Override
+            public List<String> getMatchers() {
+                return null;
+            }
+
+            @Override
+            public Map<Integer, String> getInputSchema() {
+                return null;
+            }
+
+            @Override
+            public Map<Integer, String> getOutputSchema() {
+                return null;
+            }
+
+            @Override
+            public String getProperty(String key) {
+                return new TreeMap<String, String>() {
+                    {
+                        put("nomer.eol.taxon", "/org/globalbioticinteractions/nomer/match/eol/taxon.tab");
+                    }
+                }.get(key);
+            }
+        });
+        taxonService.setTemporaryCache(true);
+        return taxonService;
+    }
+
+    @Test
+    public void parseNodes() throws PropertyEnricherException {
+        Map<String, Map<String,String>> node = new TreeMap<>();
+        Map<String, String> childParent = new TreeMap<>();
+
+        InputStream nodesStream = getClass().getResourceAsStream("/org/globalbioticinteractions/nomer/match/eol/taxon.tab");
+        EOLTaxonService.parseNodes(node, childParent, nodesStream);
+
+        assertThat(childParent.get("EOL-000000641745"), is("EOL-000000641744"));
+        Map<String, String> properties = node.get("EOL-000000641745");
+        assertThat(TaxonUtil.mapToTaxon(properties).getExternalId(), is("EOL:327955"));
+        assertThat(TaxonUtil.mapToTaxon(properties).getRank(), is("species"));
+        assertThat(TaxonUtil.mapToTaxon(properties).getName(), is("Homo sapiens"));
+
+    }
+
+    @Test
+    public void denormalizeTaxa() throws PropertyEnricherException {
+        Map<String, Map<String, String>> taxonMap = new TreeMap<String, Map<String, String>>() {{
+            TaxonImpl one = new TaxonImpl("one name", "1");
+            one.setRank("rank one");
+            put("1", TaxonUtil.taxonToMap(one));
+
+            TaxonImpl two = new TaxonImpl("two name", "2");
+            two.setRank("rank two");
+            put("2", TaxonUtil.taxonToMap(two));
+        }};
+
+        Map<String, Map<String, String>> taxonMapDenormalized = new TreeMap<>();
+
+        Map<String, String> childParent = new TreeMap<String, String>() {{
+            put("1", "2");
+        }};
+
+
+        EOLTaxonService.denormalizeTaxa(taxonMap, taxonMapDenormalized, childParent);
+
+        Taxon actual = TaxonUtil.mapToTaxon(taxonMapDenormalized.get("1"));
+        assertThat(actual.getPath(), is("two name | one name"));
+        assertThat(actual.getPathIds(), is("2 | 1"));
+        assertThat(actual.getPathNames(), is("rank two | rank one"));
+        assertThat(actual.getRank(), is("rank one"));
+        assertThat(actual.getName(), is("one name"));
+        assertThat(actual.getExternalId(), is("1"));
+
+        Taxon two = TaxonUtil.mapToTaxon(taxonMapDenormalized.get("2"));
+        assertThat(two.getPath(), is("two name"));
+        assertThat(two.getPathIds(), is("2"));
+        assertThat(two.getPathNames(), is("rank two"));
+        assertThat(two.getRank(), is("rank two"));
+        assertThat(two.getName(), is("two name"));
+        assertThat(two.getExternalId(), is("2"));
+
+    }
+
+}
