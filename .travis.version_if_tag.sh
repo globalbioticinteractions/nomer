2d69603a60b380b6d1ad12f467f9a7bca354483c
jhpoelen <jhpoelen@xs4all.nl>
2021-01-14 12:15:45 -0800

    more prep for v0.1.19

--- .travis.version_if_tag.sh
+++ .travis.version_if_tag.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [ -n "$TRAVIS_TAG" ]; then
   VERSION=${TRAVIS_TAG//[^0-9.]/}
-  mvn -pl nomer-parent versions:set -DnewVersion=$VERSION
+  mvn versions:set -DnewVersion=$VERSION
 fi
