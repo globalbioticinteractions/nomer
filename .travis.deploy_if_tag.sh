ae8ae815834cddab0731202e84af6af3ac4ea46c
jhpoelen <jhpoelen@xs4all.nl>
2021-01-14 12:58:26 -0800

    more prep for v0.1.19 release

--- .travis.deploy_if_tag.sh
+++ .travis.deploy_if_tag.sh
@@ -2,6 +2,5 @@
 if [ -n "$TRAVIS_TAG" ]; then
   mvn -s .travis.maven.settings.xml -DskipTests clean deploy
   VERSION=${TRAVIS_TAG//[^0-9.]/}
-  cp nomer/target/nomer-${VERSION}-jar-with-dependencies.jar nomer_no_head.jar
-  cat .travis.jar.magic nomer_no_head.jar > nomer.jar
+  cat .travis.jar.magic nomer/target/nomer-${VERSION}-jar-with-dependencies.jar > nomer.jar
 fi
