9c44a26672cd8facf87ccc82fc2df23a25a09435
jhpoelen <jhpoelen@xs4all.nl>
2021-08-05 12:22:45 -0700

    reproduce unexpected nullpointer exception fixed by @zedomel via https://github.com/globalbioticinteractions/nomer/pull/39

--- nomer/src/test/java/org/globalbioticinteractions/nomer/util/AppenderJSONTest.java
+++ nomer/src/test/java/org/globalbioticinteractions/nomer/util/AppenderJSONTest.java
@@ -23,4 +23,46 @@ public class AppenderJSONTest {
         assertThat(out.toString(), is("{\"norank\":{\"@id\":\"resolvedId\",\"name\":\"resolvedName\",\"equivalent_to\":{\"@id\":\"providedId\",\"name\":\"providedName\"}}}\n"));
     }
 
+    @Test
+    public void appendNullRank() {
+        String[] row = {"col1", "col2"};
+        TaxonImpl provided = new TaxonImpl("providedName", "providedId");
+        TaxonImpl resolved = new TaxonImpl("resolvedName", "resolvedId");
+        resolved.setPathNames(null);
+        resolved.setPathIds("id1 | id2");
+        resolved.setPath("name1 | name2");
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        new AppenderJSON()
+                .appendLinesForRow(
+                        row,
+                        provided,
+                        Stream.of(resolved),
+                        new PrintStream(out),
+                        taxon1 -> NameType.SAME_AS);
+
+        assertThat(out.toString(),
+                is("{\"norank\":{\"@id\":\"resolvedId\",\"name\":\"resolvedName\",\"equivalent_to\":{\"@id\":\"providedId\",\"name\":\"providedName\"}}}\n"));
+    }
+
+    @Test
+    public void appendNoRankRanks() {
+        String[] row = {"col1", "col2"};
+        TaxonImpl provided = new TaxonImpl("providedName", "providedId");
+        TaxonImpl resolved = new TaxonImpl("resolvedName", "resolvedId");
+        resolved.setPathNames("norank | norank");
+        resolved.setPathIds("id1 | id2");
+        resolved.setPath("name1 | name2");
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        new AppenderJSON()
+                .appendLinesForRow(
+                        row,
+                        provided,
+                        Stream.of(resolved),
+                        new PrintStream(out),
+                        taxon1 -> NameType.SAME_AS);
+
+        assertThat(out.toString(),
+                is("{\"norank\":{\"@id\":\"resolvedId\",\"name\":\"resolvedName\",\"equivalent_to\":{\"@id\":\"providedId\",\"name\":\"providedName\"}},\"path\":{\"names\":[\"name1\",\"name2\"],\"ids\":[\"id1\",\"id2\"],\"ranks\":[\"norank\",\"norank\"]}}\n"));
+    }
+
 }
