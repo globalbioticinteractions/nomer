f02e10d2ea98977ed6b26779efef27f52a5e8f3d
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2023-09-15 18:40:20 -0500

    fix test case

--- nomer-taxon-resolver/src/test/java/org/eol/globi/taxon/DiscoverLifeUtilTest.java
+++ nomer-taxon-resolver/src/test/java/org/eol/globi/taxon/DiscoverLifeUtilTest.java
@@ -772,9 +772,9 @@ public class DiscoverLifeUtilTest {
 
         Taxon taxon = firstTaxon.get();
 
-        assertThat(taxon.getPath(), Is.is("Animalia | Arthropoda | Insecta | Hymenoptera | Andrenidae | Acamptopoeum argentinum"));
-        assertThat(taxon.getPathIds(), Is.is("https://www.discoverlife.org/mp/20q?search=Animalia | https://www.discoverlife.org/mp/20q?search=Arthropoda | https://www.discoverlife.org/mp/20q?search=Insecta | https://www.discoverlife.org/mp/20q?search=Hymenoptera | https://www.discoverlife.org/mp/20q?search=Andrenidae | https://www.discoverlife.org/mp/20q?search=Acamptopoeum+argentinum"));
-        assertThat(taxon.getPathNames(), Is.is("kingdom | phylum | class | order | family | species"));
+        assertThat(taxon.getPath(), Is.is("Animalia | Arthropoda | Insecta | Hymenoptera | Andrenidae | Acamptopoeum | Acamptopoeum argentinum"));
+        assertThat(taxon.getPathIds(), Is.is("https://www.discoverlife.org/mp/20q?search=Animalia | https://www.discoverlife.org/mp/20q?search=Arthropoda | https://www.discoverlife.org/mp/20q?search=Insecta | https://www.discoverlife.org/mp/20q?search=Hymenoptera | https://www.discoverlife.org/mp/20q?search=Andrenidae | https://www.discoverlife.org/mp/20q?search=Acamptopoeum | https://www.discoverlife.org/mp/20q?search=Acamptopoeum+argentinum"));
+        assertThat(taxon.getPathNames(), Is.is("kingdom | phylum | class | order | family | genus | species"));
         assertThat(taxon.getName(), Is.is("Acamptopoeum argentinum"));
         assertThat(taxon.getRank(), Is.is("species"));
         assertThat(taxon.getId(), Is.is("https://www.discoverlife.org/mp/20q?search=Acamptopoeum+argentinum"));
