879879209eee75ae77e2d12119e41331f420e8e5
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-04-04 16:36:20 -0500

    towards adding subgenus for DiscoverLife taxonomy; related to https://github.com/globalbioticinteractions/nomer/issues/161

--- nomer-taxon-resolver/src/test/java/org/eol/globi/taxon/DiscoverLifeUtil2Test.java
+++ nomer-taxon-resolver/src/test/java/org/eol/globi/taxon/DiscoverLifeUtil2Test.java
@@ -3,6 +3,7 @@ package org.eol.globi.taxon;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.eol.globi.data.CharsetConstant;
 import org.eol.globi.domain.PropertyAndValueDictionary;
@@ -19,18 +20,23 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.TreeMap;
+import java.util.function.Consumer;
 
 import static org.eol.globi.service.TaxonUtil.generateTaxonPathNames;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertNotNull;
 
 public class DiscoverLifeUtil2Test {
+
+    public static final List<String> RANKS = Arrays.asList("Family", "Subfamily", "Tribe", "Subtribe", "Genus", "Subgenus");
 
     @Test
     public void compareLocalVersionToRemoteVersion() throws IOException {
@@ -43,6 +49,75 @@ public class DiscoverLifeUtil2Test {
 
     @Test
     public void parseXMLRecord() throws ParserConfigurationException, XPathExpressionException, IOException, SAXException {
+        InputStream is = getClass().getResourceAsStream("/org/globalbioticinteractions/nomer/match/discoverlife/agapostemon_texanus.xml");
+
+        Map<String, String> nameMap = parseRecord(is);
+
+        Taxon taxon = TaxonUtil.mapToTaxon(nameMap);
+        assertThat(taxon.getName(), is("Agapostemon texanus"));
+        assertThat(taxon.getRank(), is("species"));
+        assertThat(taxon.getPath(), is("Animalia | Arthropoda | Insecta | Hymenoptera | Halictidae | Halictinae | Halictini | Caenohalictina | Agapostemon | Agapostemon | Agapostemon texanus"));
+        assertThat(taxon.getPathNames(), is("kingdom | phylum | class | order | family | subfamily | tribe | subtribe | genus | subgenus | species"));
+    }
+
+    @Test
+    public void parseAcamptopoeum_melanogaster() throws ParserConfigurationException, XPathExpressionException, IOException, SAXException {
+        InputStream is = getClass().getResourceAsStream("/org/globalbioticinteractions/nomer/match/discoverlife/record_Acamptopoeum_melanogaster.xml");
+
+        Map<String, String> nameMap = parseRecord(is);
+
+        Taxon taxon = TaxonUtil.mapToTaxon(nameMap);
+        assertThat(taxon.getName(), is("Acamptopoeum melanogaster"));
+        assertThat(taxon.getRank(), is("species"));
+        assertThat(taxon.getPath(), is("Animalia | Arthropoda | Insecta | Hymenoptera | Andrenidae | Panurginae | Calliopsini | None | Acamptopoeum | None | Acamptopoeum melanogaster"));
+        assertThat(taxon.getPathNames(), is("kingdom | phylum | class | order | family | subfamily | tribe | subtribe | genus | subgenus | species"));
+    }
+
+    @Test
+    public void splitRecords() throws IOException {
+        InputStream is = getClass().getResourceAsStream("/org/globalbioticinteractions/nomer/match/discoverlife/bees-head.xml");
+
+        List<String> records = new ArrayList<>();
+        Consumer<String> recordConsumer = new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                records.add(s);
+            }
+        };
+
+        splitRecords(is, recordConsumer);
+        assertThat(records.get(0), is(IOUtils.toString(getClass().getResourceAsStream("/org/globalbioticinteractions/nomer/match/discoverlife/record_Adrenidae.xml"), StandardCharsets.UTF_8)));
+        assertThat(records.get(records.size() - 1), is(IOUtils.toString(getClass().getResourceAsStream("/org/globalbioticinteractions/nomer/match/discoverlife/record_Acamptopoeum_melanogaster.xml"), StandardCharsets.UTF_8)));
+    }
+
+    private void splitRecords(InputStream is, Consumer<String> lineConsumer) {
+        Scanner scanner = new Scanner(is, StandardCharsets.UTF_8.name());
+        while (scanner.hasNext()) {
+            String record = nextRecord(scanner);
+            if (StringUtils.isNotBlank(record)) {
+                lineConsumer.accept(record);
+            }
+        }
+    }
+
+    private String nextRecord(Scanner scanner) {
+        String record = null;
+        scanner.useDelimiter("__START__");
+        scanner.next();
+        if (scanner.hasNext()) {
+            scanner.useDelimiter("\n<set level=");
+            scanner.next();
+            if (scanner.hasNext()) {
+                scanner.useDelimiter("__STOP__");
+                record = StringUtils.trim(scanner.next());
+            }
+        }
+
+        return record;
+    }
+
+
+    private Map<String, String> parseRecord(InputStream is) throws SAXException, IOException, ParserConfigurationException, XPathExpressionException {
         Map<String, String> nameMap = new TreeMap<String, String>() {{
             put("kingdom", "Animalia");
             put("phylum", "Arthropoda");
@@ -52,7 +127,7 @@ public class DiscoverLifeUtil2Test {
         }};
 
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        Document doc = factory.newDocumentBuilder().parse(getClass().getResourceAsStream("/org/globalbioticinteractions/nomer/match/discoverlife/agapostemon_texanus.xml"));
+        Document doc = factory.newDocumentBuilder().parse(is);
 
         Node setNode = (Node) XmlUtil.applyXPath(doc, "set", XPathConstants.NODE);
 
@@ -68,11 +143,7 @@ public class DiscoverLifeUtil2Test {
 
         NodeList attr = (NodeList) XmlUtil.applyXPath(setNode, "//attributes", XPathConstants.NODESET);
 
-        assertThat(attr.getLength(), is(1));
-
         NodeList childNodes = attr.item(0).getChildNodes();
-
-        assertThat(childNodes.getLength(), is(69));
 
         String keyCurrent = "";
         ObjectNode objectNode = new ObjectMapper().createObjectNode();
@@ -88,18 +159,13 @@ public class DiscoverLifeUtil2Test {
                 if (StringUtils.isNotBlank(keyCurrent)) {
                     valuesCurrent.add(childNode.getTextContent());
                     if (valuesCurrent.size() > 0 && objectNode != null) {
-                        List<String> ranks = Arrays.asList("Family", "Subfamily", "Tribe", "Subtribe", "Genus", "Subgenus");
-                        if (ranks.contains(keyCurrent)) {
+                        if (RANKS.contains(keyCurrent)) {
                             nameMap.put(StringUtils.lowerCase(keyCurrent), valuesCurrent.get(0).asText());
                         }
                     }
                 }
             }
         }
-
-        assertNotNull(objectNode);
-
-
 
         String pathNames = generateTaxonPathNames(nameMap, Arrays.asList("kingdom", "phylum", "class", "order", "family", "subfamily", "tribe", "subtribe", "genus", "subgenus", "subspecies"), "", "genus", "specificEpithet", "subspecificEpithet", "species");
 
@@ -114,14 +180,7 @@ public class DiscoverLifeUtil2Test {
 
         String pathString = StringUtils.join(path, CharsetConstant.SEPARATOR);
         nameMap.put(PropertyAndValueDictionary.PATH, pathString);
-
-        Taxon taxon = TaxonUtil.mapToTaxon(nameMap);
-        assertThat(taxon.getName(), is("Agapostemon texanus"));
-        assertThat(taxon.getRank(), is("species"));
-        assertThat(taxon.getPath(), is("Animalia | Arthropoda | Insecta | Hymenoptera | Halictidae | Halictinae | Halictini | Caenohalictina | Agapostemon | Agapostemon | Agapostemon texanus"));
-        assertThat(taxon.getPathNames(), is("kingdom | phylum | class | order | family | subfamily | tribe | subtribe | genus | subgenus | species"));
-
-
+        return nameMap;
     }
 
     private void putTextValueForElement(Map<String, String> nameMap, Node setNode, String sourceElementName, String targetName) throws XPathExpressionException {
