634648d52fe260e06cc4adebbcf3d93344f6587b
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-06-17 19:16:10 -0500

    working towards supporting WoRMS (or any other kind of DwC-A -based checklist). Related to https://github.com/globalbioticinteractions/nomer/issues/148

---
+++ nomer-taxon-resolver/src/main/java/org/globalbioticinteractions/nomer/match/WorldRegisterOfMarineSpeciesTaxonService.java
@@ -0,0 +1,273 @@
+package org.globalbioticinteractions.nomer.match;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.RegExUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.commons.lang3.time.StopWatch;
+import org.eol.globi.domain.NameType;
+import org.eol.globi.domain.Taxon;
+import org.eol.globi.domain.TaxonImpl;
+import org.eol.globi.domain.TaxonomyProvider;
+import org.eol.globi.domain.TermImpl;
+import org.eol.globi.service.PropertyEnricherException;
+import org.eol.globi.service.TaxonUtil;
+import org.eol.globi.taxon.TaxonCacheService;
+import org.eol.globi.util.ExternalIdUtil;
+import org.globalbioticinteractions.nomer.util.TermMatcherContext;
+import org.mapdb.BTreeKeySerializer;
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+import org.mapdb.Serializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.Map;
+import java.util.Objects;
+
+public class WorldRegisterOfMarineSpeciesTaxonService extends CommonTaxonService<Long> {
+    private static final Logger LOG = LoggerFactory.getLogger(WorldRegisterOfMarineSpeciesTaxonService.class);
+
+    private static final String ACCEPTED_LABEL = "accepted";
+    private static final String SYNONYM_LABEL = "unaccepted";
+    private static final String UNCHECKED_LABEL = "UNCHECKED";
+
+
+    public WorldRegisterOfMarineSpeciesTaxonService(TermMatcherContext ctx) {
+        super(ctx);
+    }
+
+    @Override
+    public TaxonomyProvider getTaxonomyProvider() {
+        return TaxonomyProvider.WORMS;
+    }
+
+    @Override
+    public Long getIdOrNull(Taxon key, TaxonomyProvider matchingTaxonomyProvider) {
+        TaxonomyProvider taxonomyProvider = ExternalIdUtil.taxonomyProviderFor(key.getExternalId());
+        String idString = ExternalIdUtil.stripPrefix(matchingTaxonomyProvider, key.getExternalId());
+        return (matchingTaxonomyProvider.equals(taxonomyProvider)
+                && StringUtils.isNoneBlank(idString))
+                ? Long.parseLong(idString)
+                : null;
+    }
+
+    @Override
+    protected void lazyInit() throws PropertyEnricherException {
+        File taxonomyDir = new File(getCacheDir(), StringUtils.lowerCase(getTaxonomyProvider().name()));
+        DB db = DBMaker
+                .newFileDB(taxonomyDir)
+                .mmapFileEnableIfSupported()
+                .mmapFileCleanerHackDisable()
+                .compressionEnable()
+                .closeOnJvmShutdown()
+                .transactionDisable()
+                .make();
+
+        if (db.exists(NODES)
+                && db.exists(CHILD_PARENT)
+                && db.exists(MERGED_NODES)
+                && db.exists(NAME_TO_NODE_IDS)
+        ) {
+            LOG.debug("[" + getTaxonomyProvider().name() + "] taxonomy already indexed at [" + taxonomyDir.getAbsolutePath() + "], no need to import.");
+            nodes = db.getTreeMap(NODES);
+            childParent = db.getTreeMap(CHILD_PARENT);
+            mergedNodes = db.getTreeMap(MERGED_NODES);
+            name2nodeIds = db.getTreeMap(NAME_TO_NODE_IDS);
+        } else {
+            LOG.info("[" + getTaxonomyProvider().name() + "] taxonomy importing...");
+            StopWatch watch = new StopWatch();
+            watch.start();
+
+            try (InputStream resource = getClassificationStream()) {
+
+                nodes = db
+                        .createTreeMap(NODES)
+                        .keySerializer(BTreeKeySerializer.ZERO_OR_POSITIVE_LONG)
+                        .valueSerializer(Serializer.JAVA)
+                        .make();
+
+
+                childParent = db
+                        .createTreeMap(CHILD_PARENT)
+                        .keySerializer(BTreeKeySerializer.ZERO_OR_POSITIVE_LONG)
+                        .valueSerializer(Serializer.LONG)
+                        .make();
+
+                name2nodeIds = db
+                        .createTreeMap(NAME_TO_NODE_IDS)
+                        .keySerializer(BTreeKeySerializer.STRING)
+                        .valueSerializer(Serializer.JAVA)
+                        .make();
+
+                mergedNodes = db
+                        .createTreeMap(MERGED_NODES)
+                        .keySerializer(BTreeKeySerializer.ZERO_OR_POSITIVE_LONG)
+                        .valueSerializer(Serializer.LONG)
+                        .make();
+
+                NameUsageListener<Long> nameUsageListener = new NameUsageListenerImpl(mergedNodes, nodes, childParent);
+                parseNameUsage(resource, nameUsageListener);
+            } catch (IOException e) {
+                throw new PropertyEnricherException("failed to parse taxon", e);
+            }
+
+            TaxonCacheService.logCacheLoadStats(watch.getTime(), nodes.size(), LOG);
+
+            watch.stop();
+            LOG.info("[" + getTaxonomyProvider().name() + "] taxonomy imported.");
+
+        }
+    }
+
+
+    private InputStream getClassificationStream() throws PropertyEnricherException {
+        InputStream resource;
+        try {
+            resource = getCtx().retrieve(getClassificationResource());
+            if (resource == null) {
+                throw new PropertyEnricherException("World Register of Marine Species init failure: failed to find [" + getClassificationResource() + "]");
+            }
+        } catch (IOException e) {
+            throw new PropertyEnricherException("failed to parse [" + getTaxonomyProvider().name() + "]", e);
+        }
+        return resource;
+    }
+
+    private void parseNameUsage(InputStream resource, NameUsageListener<Long> nameUsageListener) throws PropertyEnricherException {
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(resource));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                parseLine(nameUsageListener, line);
+            }
+        } catch (IOException e) {
+            throw new PropertyEnricherException("failed to parse [Catalogue of Life] taxon dump", e);
+        }
+    }
+
+    private void parseLine(NameUsageListener<Long> nameUsageListener, String line) throws IOException {
+
+        JsonNode jsonNode = new ObjectMapper().readTree(line);
+
+        String taxonIdName = "http://rs.tdwg.org/dwc/terms/taxonID";
+        String taxIdString = pruneTaxonId(jsonNode, taxonIdName);
+        Long taxId = getIdAsLong(taxIdString);
+        String parentTaxIdString = pruneTaxonId(jsonNode, "http://rs.tdwg.org/dwc/terms/parentNameUsageID");
+        Long parentTaxId = getIdAsLong(parentTaxIdString);
+        String acceptedNameUsageIdString = pruneTaxonId(jsonNode, "http://rs.tdwg.org/dwc/terms/acceptedNameUsageID");
+        Long acceptedNameUsageId = getIdAsLong(acceptedNameUsageIdString);
+        String status = jsonNode.get("http://rs.tdwg.org/dwc/terms/taxonomicStatus").asText();
+        String completeName = removeQuotes(jsonNode.get("http://rs.tdwg.org/dwc/terms/scientificName").asText());
+        String authorship = removeQuotes(jsonNode.get("http://rs.tdwg.org/dwc/terms/scientificNameAuthorship").asText());
+        String rank = StringUtils.lowerCase(jsonNode.get("http://rs.tdwg.org/dwc/terms/taxonRank").asText());
+
+        String idPrefix = getTaxonomyProvider().getIdPrefix();
+        TaxonImpl taxon = new TaxonImpl(completeName, idPrefix + taxIdString);
+        if (StringUtils.isNoneBlank(authorship)) {
+            taxon.setAuthorship(StringUtils.trim(authorship));
+        }
+
+        taxon.setStatus(new TermImpl(null, status));
+
+        taxon.setRank(StringUtils.equals(StringUtils.trim(rank), "no rank") ? "" : rank);
+
+        nameUsageListener.handle(
+                status,
+                taxId,
+                Objects.equals(taxId, acceptedNameUsageId)
+                        ? parentTaxId
+                        : acceptedNameUsageId,
+                taxon
+        );
+
+
+    }
+
+    private String pruneTaxonId(JsonNode jsonNode, String taxonIdName) {
+        return StringUtils.remove(jsonNode.get(taxonIdName).asText(), "urn:lsid:marinespecies.org:taxname:");
+    }
+
+    private String removeQuotes(String rowValue) {
+        return RegExUtils.removeAll(rowValue, "(^\"|\"$)");
+    }
+
+    private static Long getIdAsLong(String stripped) {
+        return NumberUtils.isDigits(stripped) ? Long.parseLong(stripped) : null;
+    }
+
+    interface NameUsageListener<T> {
+        void handle(String status, T childTaxId, T parentTaxId, Taxon taxon);
+    }
+
+    private NameType getNameType(String statusValue) {
+        NameType nameType = NameType.NONE;
+        if (StringUtils.equalsIgnoreCase(statusValue, SYNONYM_LABEL)) {
+            nameType = NameType.SYNONYM_OF;
+        } else if (StringUtils.equalsIgnoreCase(statusValue, ACCEPTED_LABEL)) {
+            nameType = NameType.HAS_ACCEPTED_NAME;
+        } else if (StringUtils.equalsIgnoreCase(statusValue, UNCHECKED_LABEL)) {
+            nameType = NameType.HAS_UNCHECKED_NAME;
+        }
+        return nameType;
+    }
+
+    private void skipFirstLine(BufferedReader reader) throws IOException {
+        reader.readLine();
+    }
+
+    private URI getClassificationResource() {
+        String propertyValue = getCtx().getProperty("nomer.worms.url");
+        return URI.create(propertyValue);
+    }
+
+    private class NameUsageListenerImpl implements NameUsageListener<Long> {
+        private final Map<Long, Long> mergedNodes;
+        private final Map<Long, Map<String, String>> nodes;
+        private final Map<Long, Long> childParent;
+
+        public NameUsageListenerImpl(Map<Long, Long> mergedNodes,
+                                     Map<Long, Map<String, String>> nodes,
+                                     Map<Long, Long> childParent) {
+            this.mergedNodes = mergedNodes;
+            this.nodes = nodes;
+            this.childParent = childParent;
+        }
+
+        @Override
+        public void handle(String status, Long childTaxId, Long parentTaxId, Taxon taxon) {
+            registerIdForName(childTaxId, taxon, WorldRegisterOfMarineSpeciesTaxonService.this.name2nodeIds);
+
+            NameType nameType = getNameType(status);
+            if (NameType.SYNONYM_OF.equals(nameType)
+                    && childTaxId != null
+                    && parentTaxId != null) {
+                mergedNodes.put(childTaxId, parentTaxId);
+            }
+
+            if (childTaxId != null) {
+                if (NameType.HAS_UNCHECKED_NAME.equals(nameType)) {
+                    taxon.setStatus(new TermImpl(NameType.HAS_UNCHECKED_NAME.name(), NameType.HAS_UNCHECKED_NAME.name()));
+                }
+                nodes.put(childTaxId, TaxonUtil.taxonToMap(taxon));
+                if (parentTaxId != null) {
+                    childParent.put(
+                            childTaxId,
+                            parentTaxId
+                    );
+                }
+            }
+
+        }
+    }
+}
---
+++ nomer-taxon-resolver/src/test/java/org/globalbioticinteractions/nomer/match/WorldRegisterOfMarineSpeciesTaxonServiceTest.java
@@ -0,0 +1,91 @@
+package org.globalbioticinteractions.nomer.match;
+
+import org.eol.globi.domain.TaxonImpl;
+import org.eol.globi.service.PropertyEnricherException;
+import org.eol.globi.service.TaxonUtil;
+import org.eol.globi.taxon.PropertyEnricherSimple;
+import org.globalbioticinteractions.nomer.cmd.OutputFormat;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class WorldRegisterOfMarineSpeciesTaxonServiceTest {
+
+    @Rule
+
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void enrichById() throws PropertyEnricherException {
+        TaxonImpl taxon = new TaxonImpl(null, "WORMS:310843");
+        Map<String, String> enriched = ((PropertyEnricherSimple) createService()).enrich(TaxonUtil.taxonToMap(taxon));
+
+        assertThat(TaxonUtil.mapToTaxon(enriched).getExternalId(), is("WORMS:158709"));
+        assertThat(TaxonUtil.mapToTaxon(enriched).getName(), is("Ariopsis felis"));
+        assertThat(TaxonUtil.mapToTaxon(enriched).getExternalUrl(), is("https://www.marinespecies.org/aphia.php?p=taxdetails&id=158709"));
+        assertThat(TaxonUtil.mapToTaxon(enriched).getAuthorship(), is("(Linnaeus, 1766)"));
+        assertThat(TaxonUtil.mapToTaxon(enriched).getRank(), is("species"));
+        assertThat(TaxonUtil.mapToTaxon(enriched).getPath(), is("Ariopsis | Ariopsis felis"));
+        assertThat(TaxonUtil.mapToTaxon(enriched).getPathIds(), is("WORMS:158708 | WORMS:158709"));
+        assertThat(TaxonUtil.mapToTaxon(enriched).getPathNames(), is("genus | species"));
+        assertThat(TaxonUtil.mapToTaxon(enriched).getPathAuthorships(), is("Gill, 1861 | (Linnaeus, 1766)"));
+    }
+
+    @Test
+    public void enrichById2() throws PropertyEnricherException {
+        TaxonImpl taxon = new TaxonImpl(null, "WORMS:158709");
+        Map<String, String> enriched = ((PropertyEnricherSimple) createService()).enrich(TaxonUtil.taxonToMap(taxon));
+
+        assertThat(TaxonUtil.mapToTaxon(enriched).getExternalId(), is("WORMS:158709"));
+        assertThat(TaxonUtil.mapToTaxon(enriched).getName(), is("Ariopsis felis"));
+        assertThat(TaxonUtil.mapToTaxon(enriched).getExternalUrl(), is("https://www.marinespecies.org/aphia.php?p=taxdetails&id=158709"));
+        assertThat(TaxonUtil.mapToTaxon(enriched).getAuthorship(), is("(Linnaeus, 1766)"));
+        assertThat(TaxonUtil.mapToTaxon(enriched).getRank(), is("species"));
+        assertThat(TaxonUtil.mapToTaxon(enriched).getPath(), is("Ariopsis | Ariopsis felis"));
+        assertThat(TaxonUtil.mapToTaxon(enriched).getPathIds(), is("WORMS:158708 | WORMS:158709"));
+        assertThat(TaxonUtil.mapToTaxon(enriched).getPathNames(), is("genus | species"));
+        assertThat(TaxonUtil.mapToTaxon(enriched).getPathAuthorships(), is("Gill, 1861 | (Linnaeus, 1766)"));
+    }
+
+
+    private WorldRegisterOfMarineSpeciesTaxonService createService(final String nameUrl) {
+        return new WorldRegisterOfMarineSpeciesTaxonService(new TermMatcherContextClasspath() {
+            @Override
+            public String getCacheDir() {
+                try {
+                    return folder.newFolder().getAbsolutePath();
+                } catch (IOException e) {
+                    throw new RuntimeException("boom!", e);
+                }
+            }
+
+            @Override
+            public OutputFormat getOutputFormat() {
+                return null;
+            }
+
+            @Override
+            public String getProperty(String key) {
+                return new TreeMap<String, String>() {
+                    {
+                        put("nomer.worms.url", nameUrl);
+                    }
+                }.get(key);
+            }
+        });
+    }
+
+    private WorldRegisterOfMarineSpeciesTaxonService createService() {
+        return createService("/org/globalbioticinteractions/nomer/match/worms/catfish.json");
+    }
+
+
+
+}
