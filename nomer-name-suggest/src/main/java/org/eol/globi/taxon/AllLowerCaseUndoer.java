5a02185e925ea71b171b74409963c542f5a45e02
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-07-03 13:35:58 -0500

    reduce processing time of long strings with [nomer append globi-correct] related to https://github.com/globalbioticinteractions/nomer/issues/182

--- nomer-name-suggest/src/main/java/org/eol/globi/taxon/AllLowerCaseUndoer.java
+++ nomer-name-suggest/src/main/java/org/eol/globi/taxon/AllLowerCaseUndoer.java
@@ -8,14 +8,15 @@ import java.util.regex.Pattern;
 
 public class AllLowerCaseUndoer implements NameSuggester {
 
-    final private Pattern pattern = Pattern.compile("(([a-z]){2,}\\s*)+");
+    final private Pattern pattern = Pattern.compile("(?<first>[a-z]{2,})(?<remainder>.*)");
 
     @Override
     public String suggest(final String name) {
         String suggestion = name;
         Matcher matcher = pattern.matcher(name);
         if (matcher.matches()) {
-            suggestion = StringUtils.capitalize(StringUtils.lowerCase(name));
+            suggestion = StringUtils.capitalize(matcher.group("first")) +
+                    matcher.group("remainder");
         }
         return suggestion;
     }
--- nomer-name-suggest/src/test/java/org/eol/globi/taxon/AllLowerCaseUndoerTest.java
+++ nomer-name-suggest/src/test/java/org/eol/globi/taxon/AllLowerCaseUndoerTest.java
@@ -1,5 +1,7 @@
 package org.eol.globi.taxon;
 
+import org.apache.commons.lang.StringUtils;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -23,6 +25,40 @@ public class AllLowerCaseUndoerTest {
     public void doNothing() {
         String corrected = new AllLowerCaseUndoer().suggest("Homo Sapiens");
         assertThat(corrected, is("Homo Sapiens"));
+    }
+
+    @Test(timeout = 10)
+    public void longName() {
+        String aLongName = "7 small acorns or other seed found in cheek pouches during preparation. Seeds are brown, pointed at one end and flattened on the other, 0.75 x 0.6cm.";
+        String corrected = new AllLowerCaseUndoer().suggest(aLongName);
+        assertThat(corrected, is(aLongName));
+    }
+
+    @Ignore("this test fails. see https://github.com/globalbioticinteractions/nomer/issues/182")
+    @Test(timeout = 10)
+    public void capitalizeLongName() {
+        String aLongName = "acorns or   found  cheek pouches during preparation   are brown  pointed at  end  flattened  the   x  cm";
+        String corrected = StringUtils.capitalize(aLongName);
+        assertThat(corrected, is(aLongName));
+    }
+
+
+    @Test(timeout = 10)
+    public void anotherLongName() {
+        String anotherLongName = "Acorns or   found  cheek pouches during preparation   are brown  pointed at  end  flattened  the   x  cm";
+        String corrected
+                = new AllLowerCaseUndoer()
+                .suggest(anotherLongName);
+        assertThat(corrected, is(anotherLongName));
+    }
+
+    @Test(timeout = 10)
+    public void yetAnotherLongName() {
+        String anotherLongName = "Acorns or found cheek pouches during preparation are brown pointed at end flattened the x cm";
+        String corrected
+                = new AllLowerCaseUndoer()
+                .suggest(anotherLongName);
+        assertThat(corrected, is(anotherLongName));
     }
 
 }
