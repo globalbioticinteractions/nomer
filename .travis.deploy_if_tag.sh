7070590b8a7c0805a590274b09ed187ee70b6f0a
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-12-17 19:00:36 -0600

    remove unused travis config

--- .travis.deploy_if_tag.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ -n "$TRAVIS_TAG" ]; then
-  mvn -s .travis.maven.settings.xml -DskipTests clean deploy
-  VERSION=${TRAVIS_TAG//[^0-9.]/}
-  cat .travis.jar.magic nomer/target/nomer-${VERSION}-jar-with-dependencies.jar > nomer.jar
-fi
